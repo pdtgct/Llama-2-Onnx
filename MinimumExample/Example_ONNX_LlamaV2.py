4980043f2a11de1a96a5257c1d843d505b1b419f
Peter Tanski <peter.tanski@capitalone.com>
2023-08-26 17:23:02 +0000

    factor out session for reuse

--- MinimumExample/Example_ONNX_LlamaV2.py
+++ MinimumExample/Example_ONNX_LlamaV2.py
@@ -34,109 +34,112 @@ class Tokenizer:
     def decode(self, t: List[int]) -> str:
         return self.sp_model.decode(t)
 
+class LLMSession:
 
-def run_onnx_llamav2(
-    prompt: str,
-    onnx_file: str,
-    embedding_file: str,
-    tokenizer_path: str,
-    max_gen_len: int = 256,
-) -> str:
-    # Create the ONNX session
-    options = onnxruntime.SessionOptions()
-    llm_session = onnxruntime.InferenceSession(
-        onnx_file,
-        sess_options=options,
-        providers=[
-            "DmlExecutionProvider",
-            "CUDAExecutionProvider",
-            "CPUExecutionProvider",
-        ],
-    )
-
-    # get the data type used by the model
-    data_type_str = llm_session.get_inputs()[0].type
-    if data_type_str == "tensor(float16)":
-        data_type = np.float16
-    elif data_type_str == "tensor(float32)" or data_type_str == "tensor(float)":
-        data_type = np.float32
-    else:
-        raise Exception(f"Unknown data type {data_type_str}")
-
-    # Get the relevant shapes so we can create the inputs
-    for inputs_meta in llm_session._inputs_meta:
-        if inputs_meta.name == "x":
-            x_shape = inputs_meta.shape
-        elif inputs_meta.name == "attn_mask":
-            attn_mask_shape = inputs_meta.shape
-        elif inputs_meta.name == "k_cache":
-            k_cache_shape = inputs_meta.shape
-
-    hidden_size = x_shape[2]
-    max_seq_len = attn_mask_shape[1]
-    n_layers = k_cache_shape[1]
-    n_heads = k_cache_shape[3]
-
-    # Initialize the tokenizer and produce the initial tokens.
-    tokenizer = Tokenizer(model_path=tokenizer_path)
-    tokens = tokenizer.encode(prompt, bos=True, eos=False)
-
-    # create the embedding layer.
-    embedding_layer = torch.nn.Embedding(tokenizer.n_words, hidden_size)
-    embedding_layer.load_state_dict(torch.load(embedding_file))
-    embedding_layer.eval()
-
-    # Create the embeddings of the initial prompt.
-    x = embedding_layer(torch.tensor(tokens)).detach().cpu().numpy()
-    x = np.expand_dims(x, axis=0).astype(data_type)
-
-    # Create the attention mask.
-    attn_mask = -10000.0 * torch.triu(
-        torch.ones(attn_mask_shape), diagonal=1
-    ).cpu().detach().numpy().astype(data_type)
-
-    # Create the K and V caches.
-    head_dim = int(hidden_size / n_heads)
-    k_cache = np.zeros([1, n_layers, max_seq_len, n_heads, head_dim], dtype=data_type)
-    v_cache = np.zeros([1, n_layers, max_seq_len, n_heads, head_dim], dtype=data_type)
-
-    # Iteratively generate tokens.
-    pos = np.array(0)
-    output_tokens = []
-    for idx in range(max_gen_len):
-        results = llm_session.run(
-            None,
-            {
-                "x": x,
-                "attn_mask": attn_mask,
-                "k_cache": k_cache[:, :, :pos],
-                "v_cache": v_cache[:, :, :pos],
-                "pos": pos.astype(np.int64),
-            },
+    def __init__(self, onnx_file: str, embedding_file: str, tokenizer_path: str):
+        # Create the ONNX session
+        options = onnxruntime.SessionOptions()
+        self.llm_session = onnxruntime.InferenceSession(
+            onnx_file,
+            sess_options=options,
+            providers=[
+                # DML is for Windows
+                # "DmlExecutionProvider",
+                "CUDAExecutionProvider",
+                "CPUExecutionProvider",
+            ],
         )
-        logits, k_out, v_out = results[:3]
 
-        # Decide the next token using your preferred sampling strategy.
-        next_token = np.argmax(logits, axis=-1).astype(np.int64)
-        output_tokens.extend(next_token)
+        # get the data type used by the model
+        data_type_str = self.llm_session.get_inputs()[0].type
+        if data_type_str == "tensor(float16)":
+            self.data_type = np.float16
+        elif data_type_str == "tensor(float32)" or data_type_str == "tensor(float)":
+            self.data_type = np.float32
+        else:
+            raise Exception(f"Unknown data type {data_type_str}")
 
-        # Stop if/when we get an ENDOFTEXT token before reaching maximum sequence length
-        if next_token == tokenizer.eos_id:
-            break
+        # Get the relevant shapes so we can create the inputs
+        for inputs_meta in self.llm_session._inputs_meta:
+            if inputs_meta.name == "x":
+                x_shape = inputs_meta.shape
+            elif inputs_meta.name == "attn_mask":
+                attn_mask_shape = inputs_meta.shape
+            elif inputs_meta.name == "k_cache":
+                k_cache_shape = inputs_meta.shape
 
-        # Update the cache
-        seq_len = x.shape[1]
-        k_cache[:, :, pos : pos + seq_len] = k_out
-        v_cache[:, :, pos : pos + seq_len] = v_out
+        hidden_size = x_shape[2]
+        self.max_seq_len = attn_mask_shape[1]
+        n_layers = k_cache_shape[1]
+        n_heads = k_cache_shape[3]
 
-        # Update pos and x ready for the next round.
-        pos = np.array(int(pos) + seq_len, dtype=np.int64)
-        x = embedding_layer(torch.tensor(next_token)).unsqueeze(0)
-        x = x.cpu().detach().numpy().astype(data_type)
+        # Initialize the tokenizer and produce the initial tokens.
+        self.tokenizer = Tokenizer(model_path=tokenizer_path)
 
-    output_str = tokenizer.decode(torch.tensor(output_tokens).tolist())
+        # Create the attention mask.
+        self.attn_mask = -10000.0 * torch.triu(
+            torch.ones(attn_mask_shape), diagonal=1
+        ).cpu().detach().numpy().astype(self.data_type)
 
-    return output_str
+        # create the embedding layer.
+        self.embedding_layer = torch.nn.Embedding(self.tokenizer.n_words, hidden_size)
+        self.embedding_layer.load_state_dict(torch.load(embedding_file))
+        self.embedding_layer.eval()
+
+        # Create the K and V caches.
+        head_dim = int(hidden_size / n_heads)
+        self.k_cache = np.zeros([1, n_layers, self.max_seq_len, n_heads, head_dim], dtype=self.data_type)
+        self.v_cache = np.zeros([1, n_layers, self.max_seq_len, n_heads, head_dim], dtype=self.data_type)
+
+    def run_onnx(
+        self,
+        prompt: str,
+        max_gen_len: int = 256,
+    ) -> str:
+
+        tokens = self.tokenizer.encode(prompt, bos=True, eos=False)
+        # Create the embeddings of the initial prompt.
+        x = self.embedding_layer(torch.tensor(tokens)).detach().cpu().numpy()
+        x = np.expand_dims(x, axis=0).astype(self.data_type)
+
+        # Iteratively generate tokens.
+        pos = np.array(0)
+        output_tokens = []
+        for idx in range(max_gen_len):
+            results = self.llm_session.run(
+                None,
+                {
+                    "x": x,
+                    "attn_mask": self.attn_mask,
+                    "k_cache": self.k_cache[:, :, :pos],
+                    "v_cache": self.v_cache[:, :, :pos],
+                    "pos": pos.astype(np.int64),
+                },
+            )
+            logits, k_out, v_out = results[:3]
+
+            # Decide the next token using your preferred sampling strategy.
+            next_token = np.argmax(logits, axis=-1).astype(np.int64)
+            output_tokens.extend(next_token)
+
+            # Stop if/when we get an ENDOFTEXT token before reaching maximum sequence length
+            if next_token == self.tokenizer.eos_id:
+                break
+
+            # Update the cache
+            seq_len = x.shape[1]
+            self.k_cache[:, :, pos : pos + seq_len] = k_out
+            self.v_cache[:, :, pos : pos + seq_len] = v_out
+
+            # Update pos and x ready for the next round.
+            pos = np.array(int(pos) + seq_len, dtype=np.int64)
+            x = self.embedding_layer(torch.tensor(next_token)).unsqueeze(0)
+            x = x.cpu().detach().numpy().astype(self.data_type)
+
+        output_tokens = torch.tensor(output_tokens).tolist()
+        output_str = self.tokenizer.decode(output_tokens)
+
+        return output_str, output_tokens
 
 
 if __name__ == "__main__":
@@ -163,11 +166,13 @@ if __name__ == "__main__":
     )
     parser.add_argument("--max_gen_len", type=int, default=256)
     args = parser.parse_args()
-    response = run_onnx_llamav2(
-        args.prompt,
-        args.onnx_file,
+    sess = LLMSession(
+        args.onnx_file, 
         args.embedding_file,
         args.tokenizer_path,
+    )
+    response = sess.run_onnx(
+        args.prompt,
         args.max_gen_len,
     )
 
